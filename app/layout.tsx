a3e2e3245ac25993066489910847cb406c7972d6
bruhngl0 <adityashrm500@gmail.com>
2026-05-25 08:21:26 +0530

    ext

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import HUD from "./components/HUD.tsx"; // adjust path to wherever you put HUD.tsx
-import Footer from "./components/Footer.tsx";
+import HUD from "./components/HUD"; // adjust path to wherever you put HUD.tsx
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
